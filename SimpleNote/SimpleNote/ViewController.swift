a30524cbf3321b7e621308f7daebc9031ad95d43
ArjunaPratama <32661531+ArjunaPratama@users.noreply.github.com>
2017-10-24 14:39:08 +0700

    Simple Note in IOS

---
+++ SimpleNote/SimpleNote/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SimpleNote
+//
+//  Created by DOTS2 on 10/24/17.
+//  Copyright © 2017 Arjuna. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
